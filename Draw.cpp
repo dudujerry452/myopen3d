bb1d87725ddcf6d3b03816fe5603299014c36162
dudujerry452 <dudujerry452@qq.com>
2024-06-19 14:57:39 +0800

    origin art branch commit

--- Draw.cpp
+++ Draw.cpp
@@ -24,39 +24,6 @@ using std::vector;
 
 #define up(l,r,i) for(int i=l;i<=r;i++)
 
-#define chnow() std::chrono::steady_clock::now()
-#define mildiff(x) std::chrono::duration_cast<std::chrono::milliseconds>(x).count()
-
-const int64_t animation_delay = 10;
-const int resolution = 50;
-const double minError = 0.05;
-const double init_radius = 0.01;
-
-bool getData(const char * path, std::string * data){
-    std::ifstream file(path);
-    if(!file.is_open()) return false;
-    file.seekg(0, std::ios::end);
-    data->resize(file.tellg());
-    file.seekg(0, std::ios::beg);
-    file.read(&(*data)[0],data->size());
-    file.close();
-    return true;
-}
-
-float getDistanceFromEye(Eigen::Vector3d eye_position, Eigen::Vector3d target_position, double radius){
-    auto delta = target_position - eye_position;
-    double dis = std::sqrt(delta.dot(delta));
-    if(dis < radius) dis = dis + radius;
-    else dis = dis - radius;
-    return dis;
-}
-
-Eigen::Vector3d eye_pos;
-
-bool sphereCmp(const Eigen::Vector4d& s1, const Eigen::Vector4d& s2){
-    return getDistanceFromEye(eye_pos, s1.head<3>(), s1(3)) > getDistanceFromEye(eye_pos, s2.head<3>(), s2(3));
-}
-
 std::shared_ptr<LineSet> getFrameCorrdinate(double boundx, double boundy, double stepx, double stepy){
     std::vector<Eigen::Vector3d> points;
     std::vector<Eigen::Vector2i> lines;
@@ -82,178 +49,12 @@ inline double getDistance(Eigen::Vector3d o1, Eigen::Vector3d o2){
     return std::sqrt(del.dot(del));
 }
 
-bool getInteractivePart(const std::vector<Eigen::Vector3d>& geo, 
-                        Eigen::Vector3d o1, Eigen::Vector3d o2, double r1, double r2, 
-                         std::vector<Eigen::Vector3d>& result){
-    bool flag = false;
-    std::vector<Eigen::Vector3d> points;
-    std::vector<Eigen::Vector2i> lines;
-    for(const auto vertex : geo) {
-        auto err = std::abs(getDistance(o1,vertex)-r1) + std::abs(getDistance(o2,vertex)-r2);
-        if(err <= minError) {points.push_back(vertex);flag = true;}
-    }
-
-    result = points;
-    /*if(points.empty()) return flag;
-    auto first = points[0];
-    size_t len = points.size();
-    up(0,len-1,i){
-        
-
-    }*/
-    return flag;
-}
-
 int main(int argc, char *argv[]) {
 
     std::mt19937 random_generator(42);
 
-    std::string data;
-    std::vector< Eigen::Vector4d> spheres_coor; 
-
-    if(!getData("./data.dat", &data)){
-        cout <<"File invalid !" << endl;
-        return 1;
-    }
-    std::stringstream ss;
-    ss << data;
-    int n;
-    double step;
-    double k[5];//经纬高程声速单位km
-    ss>>n>>step;//输入时间增加步长，以秒为单位
-    double biggest = 0;
-    up(0,3,i) ss>>k[i];
-    k[4] = 1/k[3];
-    Eigen::Vector4d bias;
-    ss>>bias(0)>>bias(1)>>bias(2); // 输入坐标的偏移量
-    bias(3) = 0;
-    up(0,n-1,i){
-        Eigen::Vector4d v4d;
-        up(0,3,j) {
-            ss>>v4d(j);
-            v4d(j) += bias(j);
-            v4d[j] *= k[j];
-        }
-        spheres_coor.push_back(v4d);
-        biggest = (v4d(3)>biggest)?v4d(3):biggest;
-    }
-    for (auto &v : spheres_coor) v(3) -= biggest; 
-    //cube->ComputeVertexNormals();
-    //cube->PaintUniformColor({1.0, 0.0, 0.0});
-
-    std::vector< Eigen::Vector4d> points_coor;
-    int n2;
-    ss>>n2;
-    points_coor.resize(n2+1);
-    up(0, n2-1, i){
-        up(0,3,j) {
-            ss>>points_coor[i](j);
-            points_coor[i](j) += bias[j];
-            points_coor[i](j) *= k[j];
-        }
-        points_coor[i](3) -= biggest;
-    }
-
-
-    vector<std::shared_ptr<TriangleMesh> > spheres;
-    up(0,n-1,i){
-        spheres.emplace_back(TriangleMesh::CreateSphere(init_radius,resolution));
-        for (auto& v : spheres[i]->vertices_){
-            v += spheres_coor[i].head<3>();
-        }
-    }
 
     MyVisualizer scene;
-
-    std::vector<std::shared_ptr<PointCloud> > inter_points;
-
-    std::vector<std::shared_ptr<PointCloud> > draw_points_group;
-    std::vector<std::shared_ptr<PointCloud> > temp_draw_points_group;
-    // Construct points to draw
-    std::uniform_real_distribution<double> dis(0.0, 2.0*m_PI);
-    for(auto& v : points_coor){
-        std::vector<Eigen::Vector3d> v_group;
-        v_group.emplace_back(v.head<3>());
-        up(0,99,i){
-            double th = dis(random_generator);
-            double ph = dis(random_generator);
-            double co = cos(ph);
-            v_group.emplace_back(v(0)+co*sin(th),v(1)+co*cos(th),v(2)+sin(ph));
-        }
-        draw_points_group.emplace_back(std::make_shared<PointCloud>(v_group));
-        temp_draw_points_group.emplace_back(std::make_shared<PointCloud>(v_group));
-    }
-
-    std::function<bool(MyVisualizer *)> update = [&points_coor, &temp_draw_points_group, &draw_points_group, &biggest, &inter_points, &spheres,step,k,&spheres_coor,n](MyVisualizer* vis){    
-        if(!vis) return false;
-
-        double current_time = biggest*k[4] - vis->GetAnimationFrame()*step;
-        if(vis->PushKeyDown()){
-            printf("[Press \"Down\" Key] Current Time : %lf s (x%lf)\n", current_time, step);
-        }
-        if(!vis->IsNextFrame()) return false;
-
-        auto view = vis->GetViewControl();
-        eye_pos = view.GetEye().cast<double>();
-        std::sort(spheres_coor.begin(), spheres_coor.end(), sphereCmp);
-
-        up(0,n-1,i){
-
-            double radius = spheres_coor[i](3) + vis->GetAnimationFrame()*step*k[3];
-            if(radius <= 0) radius = init_radius;
-            
-            auto sph = TriangleMesh::CreateSphere(radius,resolution);
-            spheres[i]->vertices_ = sph->vertices_;
-            spheres[i]->triangles_ = sph->triangles_;
-            for(auto& v : spheres[i]->vertices_){
-                v += spheres_coor[i].head<3>();
-            }
-            spheres[i]->ComputeVertexNormals();
-            spheres[i]->ComputeTriangleNormals();
-            spheres[i]->PaintUniformColor({0,1,0});
-        }
-
-        for(const auto & remov : inter_points){ // 移除上次渲染的交线
-            vis->RemoveGeometry(remov);
-        }
-        inter_points.clear();
-
-        up(0,n-1,i){
-            double radius1 = spheres_coor[i](3) + vis->GetAnimationFrame()*step*k[3];
-
-            up(i+1,n-1,j){
-                double radius2 = spheres_coor[j](3) + vis->GetAnimationFrame()*step*k[3];
-
-                if(getDistance(spheres_coor[i].head<3>(),spheres_coor[j].head<3>()) > radius1 + radius2) continue;
-                std::vector<Eigen::Vector3d> result;
-                if(getInteractivePart(spheres[i]->vertices_, 
-                            spheres_coor[i].head<3>(), spheres_coor[j].head<3>(),
-                            radius1, radius2, 
-                             result)){
-                    inter_points.emplace_back(std::make_shared<PointCloud>(result));
-                    inter_points.back()->PaintUniformColor({0,0,0});
-                    vis->AddGeometryA(inter_points.back(), false, '0', false);
-                    
-                }
-            }
-        }
-
-        int index = 0;
-        for(auto& v : draw_points_group){
-            v->points_ = temp_draw_points_group[index]->points_;
-            v->Scale(10*sin((vis->GetAnimationFrame())/5.0),v->points_[0]);
-            if(abs(current_time - points_coor[index](3)) < step){
-                vis->AnimationPause();
-                printf("[Preset Solution Time Reached] Target Time : %lf s\n", points_coor[index](3));
-                points_coor[index](3) = 9999.0;
-            }
-            index++;
-        }
-        
-        vis->UpdateGeometry();
-        return true;
-    };
-
 
     auto coordinate_frame = TriangleMesh::CreateCoordinateFrame();
 
@@ -261,8 +62,6 @@ int main(int argc, char *argv[]) {
         std:: cout<<"[Init Error] Window Create Failed" << endl;
         return 1;
     }
-
-    //auto renderer_ptr = std::make_shared<glsl::TriangleMeshRenderer>();
 
     cout << "[Init] Window Created" << endl;
 
@@ -288,16 +87,7 @@ int main(int argc, char *argv[]) {
 
     cout << "[Init] Add Frame Corrdinate Succeed " << endl;
 
-    up(0,n-1,i){
-        scene.AddGeometry(spheres[i]);
-    }
-    cout << "[Init] Add Custom Balls Succeed " << endl;
-
-    up(0,n2-1,i){
-        scene.AddGeometryA(draw_points_group[i], false, '0', true);
-    }
-
-    scene.RegisterAnimationCallback(update);
+    //scene.RegisterAnimationCallback(update);
     cout << "[Init] Animation Function Register Succeed " << endl;
     scene.Run();
     scene.DestroyVisualizerWindow();
--- include/m_util.h
+++ include/m_util.h
@@ -7,3 +7,6 @@ inline m_T const m_max(const m_T& _l, const m_T& _r){ return (_l>_r)?_l:_r;}
 
 template<typename m_T>
 inline m_T const m_min(const m_T& _l, const m_T& _r){ return (_l<_r)?_l:_r;}
+
+#define chnow() std::chrono::steady_clock::now()
+#define mildiff(x) std::chrono::duration_cast<std::chrono::milliseconds>(x).count()
